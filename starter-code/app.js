5dacfed5a267ead980dd84018777910ef744d248
Jean Nothomb <jeannothomb@gmail.com>
2017-11-13 20:29:33 +0100

    completing linter and modules

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,10 +1,12 @@
-const express      = require('express');
-const path         = require('path');
-const favicon      = require('serve-favicon');
-const logger       = require('morgan');
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const mongoose     = require("mongoose");
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -22,7 +24,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('combined'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ starter-code/models/course.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+  name: String,
+  startingDate: String,
+  endDate: String,
+  level: String,
+  available: Boolean
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+const Course = mongoose.model("Course", courseSchema);
+module.exports = Course;
